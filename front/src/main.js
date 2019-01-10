833c6ce5d1a2697bb9669a8eb0c4bdfa1d6a899e
Maksim Gachevskiy <hektodaleky@gmail.com>
2019-01-10 21:03:01 +0600

    Vue router

--- front/src/main.js
+++ front/src/main.js
@@ -1,15 +1,33 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-Vue.use(Vuex);
-window.Vue = require('vue');
 import {store} from './store/store';
 import AppLayout from './App.vue';
+import NotFound from './components/NotFound/NotFound'
+
+Vue.use(Vuex);
+window.Vue = require('vue');
 
 
 Vue.component('example-component', AppLayout);
 
-const app = new Vue({
+
+const routes = {
+    '/': AppLayout,
+    '*': NotFound
+}
+
+new Vue({
     el: '#app',
+    data: {
+        currentRoute: window.location.pathname
+    },
+    computed: {
+        ViewComponent() {
+            return routes[this.currentRoute] || NotFound
+        }
+    },
     store,
-    render: h => h(AppLayout)
+    render(h) {
+        return h(this.ViewComponent)
+    }
 });
